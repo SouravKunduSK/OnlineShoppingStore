0f1ce633ee899813a6804055f667caa9be9f7433
sohan2133 <sohan.just.cse19@gmail.com>
2021-04-12 10:17:03 +0600

    180138

---
+++ 180138/Controllers/AdminController.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using Online_Shopping.DAL;
+using Online_Shopping.Models;
+using Online_Shopping.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Online_Shopping.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allcategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(allcategories);
+        }
+        public ActionResult AddCategory()
+        {
+            return UpdateCategory(0);
+        }
+
+        public ActionResult UpdateCategory(int categoryId)
+        {
+            CategoryDetails cd;
+            if (categoryId != null)
+            {
+                cd = JsonConvert.DeserializeObject<CategoryDetails>(JsonConvert.SerializeObject(_unitOfWork.GetRepositoryInstance<Tbl_Category>().GetFirstorDefault(categoryId)));
+            }
+            else
+            {
+                cd = new CategoryDetails();
+            }
+            return View("UpdateCategory", cd);
+
+        }
+        public ActionResult Product()
+        {
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Product>().GetProduct());
+        }
+        public ActionResult ProductEdit(int productId)
+        {
+            return View(_unitOfWork.GetRepositoryInstance<Tbl_Product>().GetFirstorDefault(productId));
+        }
+        [HttpPost]
+        public ActionResult ProductEdit(Tbl_Product tbl)
+        {
+            _unitOfWork.GetRepositoryInstance<Tbl_Product>().Update(tbl);
+            return RedirectToAction("Product");
+        }
+        public ActionResult ProductAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ProductAdd(Tbl_Product tbl)
+        {
+            _unitOfWork.GetRepositoryInstance<Tbl_Product>().Add(tbl);
+            return RedirectToAction("Product");
+        }
+    }
+
+}
