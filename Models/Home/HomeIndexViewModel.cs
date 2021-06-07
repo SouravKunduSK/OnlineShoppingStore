3ebc1639b95d675b801a39c575631201a7cad788
Md. Rasel Hossain <59763029+Rasel66@users.noreply.github.com>
2021-06-07 10:59:25 +0600

    180118

---
+++ Models/Home/HomeIndexViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Ecommerce_site.DAL;
+using Ecommerce_site.Repository;
+using System;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Ecommerce_site.Models.Home
+{
+    public class HomeIndexViewModel
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        dbAPESEntities context = new dbAPESEntities();
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+        public HomeIndexViewModel CreateModel(string search, int pageSize, int? page)
+        {
+            SqlParameter[] param = new SqlParameter[]{
+                new SqlParameter("@search",search??(object)DBNull.Value)
+            };
+            IPagedList<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList().ToPagedList(page ?? 1, pageSize);
+            return new HomeIndexViewModel
+            {
+                ListOfProducts = data
+            };
+        }
+    }
+}
